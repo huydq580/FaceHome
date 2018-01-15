80b79fde5628d56afd9624c9b117e814aa97a98d
hieuuet <failure95nd@gmail.com>
2018-01-15 17:43:28 +0700

    canh bao chay nhanh

--- src/containers/banquanli/menuleft/CanhBaoChayNhanh.js
+++ src/containers/banquanli/menuleft/CanhBaoChayNhanh.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import {
     View,
-    Text
+    Text,
+    TextInput,
+    StyleSheet
 } from 'react-native';
 
 export default class CanhBaoChayNhanh extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            CanhBao: '',
+        }
+    }
     render(){
         return(
             <View>
-                <Text>Canh bao chay nhanh</Text>
+                <View style = {styles.itemBoder}>
+                    <TextInput placeholder = 'Ban quản lí nhập thông tin tại đây và bấm nút báo chay bến duwois để thông báo nhanh tới toàn bộ dân cư trong KĐT'
+                               underlineColorAndroid="transparent"
+                               onChangeText ={(CanhBao)=> this.setState({CanhBao)}/>
+                </View>
+                <View style = {{justifyContent:'center', marginTop:20}}>
+                    <Text style = {{borderWidth:1, backgroundColor:'red'}}>Báo Cháy</Text>
+                </View>
+
+                <View>
+                    <Text>Hoặc gọi ngay cứu hỏa: </Text>
+                    <Text>114</Text>
+                </View>
+                <Text>Lịch sử báo cháy</Text>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    itemBoder: {
+        borderWidth:1,
+        marginHorizontal: 30,
+        marginTop:20,
+    },
+})
