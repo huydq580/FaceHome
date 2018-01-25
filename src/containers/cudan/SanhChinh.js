b1f13ddcbfc5e9ab5d0f8273ec3167bf6842f752
hieuuet <failure95nd@gmail.com>
2018-01-25 10:01:16 +0700

    dan cu

--- src/containers/cudan/SanhChinh.js
+++ src/containers/cudan/SanhChinh.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
+    Image,
+    TouchableOpacity,
+    FlatList,
+    ScrollView
 } from 'react-native'
+import stylesContainer from "../../components/style";
+import StatusItems from "../../components/StatusItems";
 
 export default class SanhChinh extends Component {
     constructor(props){
@@ -10,15 +16,56 @@ export default class SanhChinh extends Component {
         this.state = {
             dataBanTin: [''],
             dataBaiDang: [''],
+            dataItem :
+                [
+                    {
+                        "status": "Một con vit xòe ra 2 cái cánh",
+                        "like": "11",
+                        'comment': '30',
+                    },
+                    {
+                        "status": "Bà ơi bà cháu yêu bà lắm, tóc bà trắng màu trắng màu trắng như mây, cháu yêu bà cháu nắm bàn tay.",
+                        "like": "55",
+                        'comment': '20',
+                    },
+                    {
+                        "status": "wtf",
+                        "like": "66",
+                        'comment': '10',
+                    },
+
+                ],
         }
     }
     render(){
         return(
-            <View>
-                <Text>Thông Tin Từ Ban Quản Lý</Text>
-                
-                
-            </View>
+            <ScrollView style = {stylesContainer.container}>
+                <View>
+                    <View style  = {{flexDirection:'row', marginTop: 15}}>
+                        <Image source={require('../../images/chieu-cao-va-tieu-su-cua-phuong-ly-12-e1482887471940.jpg')}
+                               style = {{ resizeMode: 'cover',height: 40, width:30, marginLeft:10}}>
+                        </Image>
+                        <View style = {{marginLeft: 10, borderWidth: 1, borderColor: '#cccccc', borderRadius:20, flex:1,justifyContent:'center' ,alignItems:'center'}}>
+                            <TouchableOpacity onPress = {()=>this.props.navigation.navigate('SoanTin')}>
+                                <Text>Soạn đăng bản tin cho KĐT</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                    </View>
+                </View>
+                <View style={{height: 3, backgroundColor: '#cccccc', marginTop: 10}}/>
+                <FlatList
+                    data = {this.state.dataItem}
+                    renderItem={(item) => {
+                        return (
+                            <StatusItems
+                                dataItem={item}/>
+                        )
+                    }
+                    }
+                    keyExtractor={(item, index) => index}
+                />
+            </ScrollView>
         )
     }
 }
--- src/containers/cudan/menuleft/NhaCuDan.js
+++ src/containers/cudan/menuleft/NhaCuDan.js
@@ -5,17 +5,47 @@ import {
     StyleSheet,
     TouchableOpacity,
     TextInput,
-    AsyncStorage
+    AsyncStorage,
+    FlatList,
+    ScrollView,
+    Image
 } from 'react-native';
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { NavigationActions } from 'react-navigation'
 import stylesContainer from "../../../components/style";
 import {callApiNhaCuDan} from "../../../actions/actionsCuDan/NhaCuDanActions";
+import StatusItems from "../../../components/StatusItems";
 
 
 
 class NhaCuDan extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            dataItem :
+                [
+                    {
+                        "status": "Một con vit xòe ra 2 cái cánh",
+                        "like": "11",
+                        'comment': '30',
+                    },
+                    {
+                        "status": "Bà ơi bà cháu yêu bà lắm, tóc bà trắng màu trắng màu trắng như mây, cháu yêu bà cháu nắm bàn tay.",
+                        "like": "55",
+                        'comment': '20',
+                    },
+                    {
+                        "status": "wtf",
+                        "like": "66",
+                        'comment': '10',
+                    },
+
+                ],
+
+        }
+    }
+
     componentWillMount() {
         const { UserCuDan } = this.props;
         if (UserCuDan.length <= 0) {
@@ -37,7 +67,7 @@ class NhaCuDan extends Component {
         }
         // console.log('infoBQL', infoCuDan[0].FullName)
         return (
-            <View style = {stylesContainer.container}>
+            <ScrollView style = {stylesContainer.container}>
                 <View style = {{flexDirection:'row', alignItems:'center'}}>
                     <View style = {styles.circle}>
                         <Text>Avatar</Text>
@@ -64,26 +94,37 @@ class NhaCuDan extends Component {
                         <Text>{infoCuDan[0].Phone}</Text>
                     </View>
                 </View>
-                <View style = {styles.viewItem}>
-                    <Text style = {{flex:5}}>Bạn có muốn đăng bài lên bản tin không?</Text>
-                    <View style = {{flex:1,borderWidth:1, backgroundColor: '#42A5F5', width:100,height:40,
-                        alignItems:'center', justifyContent:'center'}}>
-                        <Text>Đăng</Text>
+                <View style={{height: 1, backgroundColor: '#cccccc', marginTop: 20}}/>
+                <View>
+                    <View style  = {{flexDirection:'row', marginTop: 20}}>
+                        <Image source={require('../../../images/chieu-cao-va-tieu-su-cua-phuong-ly-12-e1482887471940.jpg')}
+                               style = {{ resizeMode: 'cover',height: 40, width:30, marginLeft:10}}>
+                        </Image>
+                        <View style = {{marginLeft: 10, borderWidth: 1, borderColor: '#cccccc', borderRadius:20, flex:1,justifyContent:'center' ,alignItems:'center'}}>
+                            <TouchableOpacity onPress = {()=>this.props.navigation.navigate('SoanTin')}>
+                                <Text>Soạn đăng bản tin cho KĐT</Text>
+                            </TouchableOpacity>
+                        </View>
+
                     </View>
                 </View>
-                <View style = {styles.viewItem}>
-                    <Text>Bài đăng 1</Text>
-                </View>
-                <View style = {styles.viewItem}>
-                    <Text>Bài đăng 2</Text>
-                </View>
-                <View style = {styles.viewItem}>
-                    <Text>Bài đăng 3</Text>
-                </View>
+                <View style={{height: 3, backgroundColor: '#cccccc', marginTop: 10}}/>
+                <FlatList
+                    data = {this.state.dataItem}
+                    renderItem={(item) => {
+                        return (
+                            <StatusItems
+                                dataItem={item}/>
+                        )
+                    }
+                    }
+                    keyExtractor={(item, index) => index}
+                />
 
 
 
-            </View>
+
+            </ScrollView>
         )
     }
 }
