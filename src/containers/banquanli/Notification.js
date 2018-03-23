5b612b8f451b73896f2ee596dfd0098f1ceab7e2
hieuuet <anhhieuuet@gmail.com>
2018-03-23 09:36:35 +0700

    rr

--- src/containers/banquanli/Notification.js
+++ src/containers/banquanli/Notification.js
@@ -2,32 +2,92 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
+    TouchableOpacity,
+    FlatList,
     Button
 } from 'react-native';
+import Dimensions from 'Dimensions';
+const DEVICE_WIDTH = Dimensions.get('window').width;
+import Icon from 'react-native-vector-icons/dist/Entypo'
+import ThongBaoItem from '../../components/thongbao/ThongBaoItem';
 
 export default class Notification extends Component {
     static navigationOptions = ({navigation}) => {
         const {state} = navigation;
         return {
-            headerRight: <Button onPress={() => {navigation.navigate('TinNhanBQL')}}
-                                 title = 'Tin nhắn'
-                                 style = {{marginRight:10}}/>
+            headerRight: <Button onPress={() => {
+                navigation.navigate('TinNhanBQL')
+            }}
+                                 title='Tin nhắn'
+                                 style={{marginRight: 10}}/>
         }
     }
-    constructor(props) {
+
+
+    constructor(props){
         super(props)
+
         this.state = {
-            status : 2
+            listNoti:[
+                {
+                    img:'',
+                    title:'title1',
+                    body:'body1',
+                    content:'abcd',
+                    isSeen:false,
+                    time:'2017-03-01 8:00'
+                },
+                {
+                    img:'',
+                    title:'title1',
+                    body:'body1',
+                    content:'abcd',
+                    isSeen:true,
+                    time:'2017-03-01 8:00'
+                },{
+                    img:'',
+                    title:'title1',
+                    body:'body1',
+                    content:'abcd',
+                    isSeen:false,
+                    time:'2017-03-01 8:00'
+                }
+            ]
         }
+
     }
     render (){
+        const {navigation} = this.props;
         return (
-           <View>
-               <Text>hihi</Text>
-               {
+            <View style={{flex:1}}>
+                <FlatList
+                    data={this.state.listNoti}
+                    renderItem={(item) => {
+                        return (
+                            <ThongBaoItem
+                                dataItem={item}
+                                navigation={navigation}
+                            />
+                        )
+                    }}
+                    keyExtractor={(item, index) => index.toString()}
+                    ItemSeparatorComponent={this.renderSeparator}
+                />
+            </View>
+        );
+    }
 
-               }
-           </View>
+
+    renderSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: "86%",
+                    backgroundColor: "#CED0CE",
+                    marginLeft: DEVICE_WIDTH / 5
+                }}
+            />
         );
     }
 }
