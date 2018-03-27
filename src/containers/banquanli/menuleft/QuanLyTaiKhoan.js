8b6a0e6859c945714411432a3a98cc543b0d76b6
hieuuet <anhhieuuet@gmail.com>
2018-03-27 12:32:27 +0700

    dang xuat tai khoan

--- src/containers/banquanli/menuleft/QuanLyTaiKhoan.js
+++ src/containers/banquanli/menuleft/QuanLyTaiKhoan.js
@@ -7,19 +7,37 @@ import {
 } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import stylesContainer from "../../../components/style";
+import Modal from "react-native-modal";
 
 export default class QuanLyTaiKhoan extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            visibleModal: null
+        };
+        this.Logout = this.Logout.bind(this)
+    }
+
+    // _renderButton = (text, onPress) => (
+    //     <TouchableOpacity onPress={onPress}>
+    //         <View style={styles.button}>
+    //             <Text>{text}</Text>
+    //         </View>
+    //     </TouchableOpacity>
+    // );
+
+    // _renderModalContent = () => (
+    //     <View style={styles.modalContent}>
+    //         <TouchableOpacity OnPress = {this.Logout}>
+    //             <Text>Đăng xuất</Text>
+    //         </TouchableOpacity>
+    //         {this._renderButton("Close", () => this.setState({ visibleModal: null }))}
+    //     </View>
+    // );
+
     Logout() {
-        AsyncStorage.removeItem('UserID')
-        const resetAction = NavigationActions.reset({
-            index: 0,
-            actions: [
-                NavigationActions.navigate({
-                    routeName: 'DangNhap',
-                }),
-            ]
-        });
-        this.props.navigation.dispatch(resetAction)
+        console.log('hihi')
+
     }
     render (){
         return (
@@ -34,7 +52,7 @@ export default class QuanLyTaiKhoan extends Component {
                         </Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity onPress = {this.Logout.bind(this)}>
+                <TouchableOpacity onPress = {()=> this.setState({ visibleModal: 5 })}>
                     <View style = {styles.viewitem}>
                         <Text style = {{textDecorationLine: "underline", marginTop:10, color:'black', marginLeft:10, }}>
                             Đăng xuất
@@ -44,6 +62,41 @@ export default class QuanLyTaiKhoan extends Component {
                         </Text>
                     </View>
                 </TouchableOpacity>
+                {/*{this._renderButton("Bottom half modal", () =>*/}
+                    {/*this.setState({ visibleModal: 5 })*/}
+                {/*)}*/}
+                <Modal
+                    isVisible={this.state.visibleModal === 5}
+                    style={styles.bottomModal}
+                >
+                   <View>
+                       <TouchableOpacity onPress =  {()=> {AsyncStorage.removeItem('UserID')
+                           const resetAction = NavigationActions.reset({
+                               index: 0,
+                               actions: [
+                                   NavigationActions.navigate({
+                                       routeName: 'DangNhap',
+                                   }),
+                               ]
+                           });
+                           this.props.navigation.dispatch(resetAction)}}>
+                           <View style = {styles.modalContent}>
+                               <Text style = {{fontSize:11}}>Bạn có muốn đăng xuất tài khoản này?</Text>
+                               <View style = {{height:1, backgroundColor: 'red'}}/>
+                               <Text style = {{color: 'red', fontSize:18, marginTop: 15}}>
+                                   Đăng xuất
+                               </Text>
+                           </View>
+                       </TouchableOpacity>
+                       <TouchableOpacity onPress = { ()=> this.setState({ visibleModal: null })}>
+                           <View style = {[styles.modalContent, {marginTop: 10}]}>
+                               <Text style = {{fontSize:18, color: '#2196F3'}}>
+                                   Hủy
+                               </Text>
+                           </View>
+                       </TouchableOpacity>
+                   </View>
+                </Modal>
             </View>
         )
     }
@@ -54,5 +107,28 @@ const styles = StyleSheet.create({
         marginHorizontal:20,
         borderColor:'#BDBDBD'
 
+    },
+    modalContent: {
+        flexDirection:'column',
+        backgroundColor: "white",
+        padding: 22,
+        // justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+        marginHorizontal: 10,
+        borderColor: "rgba(0, 0, 0, 0.1)"
+    },
+    button: {
+        backgroundColor: "lightblue",
+        padding: 12,
+        margin: 16,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 4,
+        borderColor: "rgba(0, 0, 0, 0.1)"
+    },
+    bottomModal: {
+        justifyContent: "flex-end",
+        margin: 0
     }
 })
